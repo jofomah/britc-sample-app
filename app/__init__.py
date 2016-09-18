eab47be9b5cdb278684bf00fbdfc5a6582034c9c
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-09-18 21:07:29 +0100

    load client and feature request routes modules

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,10 @@ app = Flask(__name__, static_url_path='')
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+
+from app.models import client
+from app.models import feature_request
 from app.routes import index
+
+from app.routes import clients
+from app.routes import feature_requests
