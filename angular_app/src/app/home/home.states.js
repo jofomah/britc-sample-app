43234df2ccfbc7dce360204c1c44e8fcab1d81e1
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-09-18 21:10:50 +0100

    fetch features list on state home resolve block.

--- angular_app/src/app/home/home.states.js
+++ angular_app/src/app/home/home.states.js
@@ -1,13 +1,19 @@
 'use strict'
 
 angular.module('home')
-  .config(function ($stateProvider) {
-    $stateProvider.state('home', {
-      parent: 'index',
-      url: '/',
-      templateUrl: 'app/home/home.tpl.html',
-      data: {
-        label: 'Home'
-      }
+    .config(function ($stateProvider) {
+        $stateProvider.state('home', {
+            parent: 'index',
+            url: '/',
+            templateUrl: 'app/home/home.tpl.html',
+            controller: "HomeController as homeCtrl",
+            resolve: {
+                featureRequests: function (FeatureService) {
+                    return FeatureService.getList()
+                }
+            },
+            data: {
+                label: 'Home'
+            }
+        })
     })
-  })
