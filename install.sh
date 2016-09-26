fadf3bc133bc3354b32a9c5231e17510c5fa3270
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-09-26 16:11:09 +0100

    update install dependencies

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ flask/bin/pip install setuptools --no-binary --upgrade
 flask/bin/pip install flask==0.9
 flask/bin/pip install flask-login
 flask/bin/pip install flask-openid
-flask/bin/pip install flask-mail==0.7.6
+flask/bin/pip install flask-mail
 flask/bin/pip install sqlalchemy==0.7.9
 flask/bin/pip install flask-sqlalchemy==0.16
 flask/bin/pip install sqlalchemy-migrate==0.7.2
@@ -13,9 +13,7 @@ flask/bin/pip install pytz==2013b
 flask/bin/pip install flask-babel==0.8
 flask/bin/pip install flup
 flask/bin/pip install nose==1.3.7
-
-flask/bin/pip install authomatic
+flask/bin/pip install flask-user
 flask/bin/pip install flask-admin
-flask/bin/pip install flask-security
-flask/bin/pip install flask-restful
+
 
