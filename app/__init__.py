b3083b8ca7be833629cf85ba1dcf1b2a66dda7bc
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-09-27 02:16:02 +0100

    pep 8 formatting

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ from app.routes import index
 from app.routes import clients
 from app.routes import feature_requests
 
-#import views
+# import views
 from app.views import admin_views
 
 # Setup Flask-Security
@@ -29,6 +29,8 @@ security = Security(app, user_datastore)
 mail = Mail(app)
 
 # Create admin user on start up
+
+
 @app.before_first_request
 def create_user():
     admin_config = app.config.get('ADMIN')
@@ -37,14 +39,15 @@ def create_user():
     email = admin_config['email']
     if not user.User.query.filter(user.User.username == username).first():
         admin_user = user.User(username=username, email=email, is_enabled=True,
-                     password=encrypt_password(password))
+                               password=encrypt_password(password))
 
         admin_user.roles.append(user.Role(name='admin'))
         db.session.add(admin_user)
         db.session.commit()
 
 # add models to be managed via Flask Admin iinterface
-admin = Admin(app, name='britc-sample-app', base_template='my_master.html', template_mode='bootstrap3')
+admin = Admin(app, name='britc-sample-app',
+              base_template='my_master.html', template_mode='bootstrap3')
 admin.add_view(admin_views.MyModelView(user.User, db.session))
 admin.add_view(admin_views.MyModelView(client.Client, db.session))
 admin.add_view(admin_views.MyModelView(user.Role, db.session))
