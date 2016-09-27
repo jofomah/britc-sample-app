2b3738cfb0ce171bb71aeea65bcbba759c97a263
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-09-27 02:01:30 +0100

    remove debugging log

--- app/views/admin_views.py
+++ app/views/admin_views.py
@@ -1,6 +1,6 @@
 from flask import url_for, abort, request, redirect
 from flask_admin.contrib.sqla import ModelView
-from flask_user import current_user
+from flask_security import current_user
 
 
 # Create customized model view class
@@ -10,7 +10,6 @@ class MyModelView(ModelView):
         return current_user and current_user.is_authenticated and current_user.has_role('admin')
 
     def _handle_view(self, name, **kwargs):
-        print url_for('user.logout', next=request.url), url_for('user.login', next=request.url)
         """
         Override builtin _handle_view in order to redirect users when a view is not accessible.
         """
@@ -20,4 +19,4 @@ class MyModelView(ModelView):
                 abort(403)
             else:
                 # login
-                return redirect(url_for('user.login', next=request.url))
+                return redirect(url_for('security.login', next=request.url))
