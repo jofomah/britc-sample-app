95f07bd13c2001501371cd52bc20ac562b58e40f
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-09-27 16:57:29 +0100

    add function for sorting features by client and priortiy. then placeholders for persisting after sorting

--- angular_app/src/app/components/feature-request/feature-request.service.js
+++ angular_app/src/app/components/feature-request/feature-request.service.js
@@ -1,7 +1,7 @@
 'use strict'
 
 angular.module('feature')
-    .service('FeatureService', function ($q, config, $http, $filter) {
+    .service('FeatureService', function ($q, config, $http, $filter, $rootScope) {
         var self = this
         var featureUrl = [config.api.url, config.api.feature].join('/')
 
@@ -27,7 +27,6 @@ angular.module('feature')
         }
 
         self.save = function (record) {
-            console.log(record)
             var featureRequest = angular.copy(record)
             if (featureRequest.targetDate) {
                 featureRequest.targetDate = $filter('date')(featureRequest.targetDate, 'yyyy-MM-dd')
@@ -51,11 +50,34 @@ angular.module('feature')
 
         self.deleteBy = function (id) {
             var url = [featureUrl, id].join('/')
-            console.log('Delete : ', url)
             return $http.delete(url)
                 .then(function (response) {
                     return response.data
                 })
+        }
+
+        self.sortFeatureBy = function (features) {
+            return features.sort(function (featA, featB) {
+                // sort by client name, then priority in ASC order, i.e if 0, same client name, sort by priority
+                return featA.client.name.localeCompare(featA.client.name) || (featA.priority - featB.priority)
+            })
+        }
+
+        self.filterBy = function (list, clientId) {
+            return list.filter(function (item) {
+                return item.client.id === clientId
+            })
+        }
+
+        self.rearrange = function (srcIndex, destIndex, movedFeature, oldList, updatedList) {
+            var selectedFeatureClientId =  (movedFeature.client && movedFeature.client.id) || 'unknown'
+            var sameClientOldList = self.filterBy(oldList, selectedFeatureClientId)
+            var sameClientUpdatedList = self.filterBy(updatedList, selectedFeatureClientId)
+
+            // TODO: implement and prepare data to be sent to the server
+            $rootScope.$applyAsync(function () {
+                // TODO: trigger loading overlay to prevent concurrent sorting, hide overlay after current sort has been saved in the server successfully
+            })
         }
 
     })
